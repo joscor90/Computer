99650175e04089c231b2acd2658de9a7e834dd18
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-09 08:55:56 -0300

    Added class variable users to Computer class

--- computer.rb
+++ computer.rb
@@ -1,8 +1,10 @@
 class Computer
+    @@users = {}
     def initialize(username, password)
         @username = username
         @password = password
         @files = {}
+        @@users[username] = password
     end
 end
 
